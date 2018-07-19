c48f05fab8d950d2416b3b13df3bbb1c0016b3c6
Jessica Dussault <jdussault4@gmail.com>
2018-07-19 13:51:15 -0500

    very basic implementation of navigation and facets

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  langs = APP_OPTS["languages"]
+  locales = langs ? Regexp.new(langs) : /en/
+
+  # make sure all new routes can be english or spanish
+  scope "(:locale)", locale: locales do
+
+    # about
+    get '/about/people', to: 'general#about_people', as: 'about_people'
+
+    # explore / explora
+    get '/explore', to: 'explore#index', as: 'explore'
+
+    # research / investiga
+    get '/research', to: 'research#index', as: 'research'
+
+    # teach / enseña
+    get '/teach', to: 'teach#index', as: 'teach'
+  end
+
 end
