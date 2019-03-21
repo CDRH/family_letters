30c62979e73bddce0604b17bdb5754d80822ea11
Karin Dalziel <kdalziel2@unl.edu>
2019-03-21 15:16:45 -0500

    added isabels content

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,6 +1,17 @@
 class ExploreController < ApplicationController
 
+  before_action :load_query
+
   def index
+  end
+
+  def exhibit_02
+  end
+
+  private
+
+  def load_query
+    @res = $api.query({"q" => "water", "num" => "5"})
   end
 
 end
