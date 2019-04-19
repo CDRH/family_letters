bf01ffe21a3762aec8f8f26439c4ec86ef596bd7
Jessica Dussault <jdussault4@gmail.com>
2019-04-19 12:25:31 -0500

    overrides item title by language
    requires https://github.com/CDRH/orchid/pull/112 for functionality

--- app/controllers/items_override.rb
+++ app/controllers/items_override.rb
@@ -5,6 +5,7 @@ ItemsController.class_eval do
     options, @from, @to = helpers.date_filter(options)
     search_language(options)
 
+    @title = t "search.title"
     @res = $api.query(options)
   end
 
@@ -19,6 +20,17 @@ ItemsController.class_eval do
         options["q"] = "(text_t_es:#{query}) OR (text_t_en:#{query})"
       end
       options
+    end
+  end
+
+  private
+
+  def item_title
+    title_field = params["locale"] == "en" ? "title" : "title_es_k"
+    if @res[title_field].present?
+      @res[title_field].length > 20 ? "#{@res[title_field][0,20]}..." : @res[title_field]
+    else
+      "Item #{@res["identifier"]}"
     end
   end
 end
