bb475eca0be283486b8395b74930c0093fb8a1e4
Jessica Dussault <jdussault4@gmail.com>
2019-10-16 11:51:42 -0500

    fixes links for webscraping and facet change to original language

--- app/controllers/general_override.rb
+++ app/controllers/general_override.rb
@@ -6,14 +6,17 @@ GeneralController.class_eval do
       route.path.spec.to_s
     end
     filtered = all_routes.select { |r| r[/explore|research|teach/] }
+    # get the current URL and remove this path from it
+    # because request.base_url will not accommodate sub-uris
+    base_url = request.url.sub("/content_pages", "")
     urls = filtered.map do |path|
       # family letters only supports en as an alternate language
       # but if this changes we can pull languages from the config file
       path.gsub!("(.:format)", "").gsub!("(/:locale)", "")
       # group the urls with different language but same content
       [
-        File.join(request.base_url, path),
-        File.join(request.base_url, "en", path)
+        File.join(base_url, path),
+        File.join(base_url, "en", path)
       ]
     end
     render json: urls
