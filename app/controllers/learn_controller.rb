3ea1af1b836c7f088541571655af43c011581518
Jessica Dussault <jdussault4@gmail.com>
2019-10-31 11:12:17 -0500

    adds the rest of the lesson documents / images
    had to do some custom work for lesson 5
    so now you can pass an additional image and a block

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -6,26 +6,45 @@ class LearnController < ApplicationController
   end
 
   def lesson01
-    @title = t "learn.lesson01.title"
     @xmas = get_item("shan_p.135")
     @tv = get_item("shan_p.153")
     @stamps = get_item("shan_m.026")
     @letter = get_item("shan_L094.meta")
+
+    @title = t "learn.lesson01.title"
   end
 
   def lesson02
+    @wed1 = get_item("shan_p.257")
+    @wed2 = get_item("shan_p.073")
+    @wed3 = get_item("shan_p.067")
+    @letter = get_item("shan_L218.meta")
+
     @title = t "learn.lesson02.title"
   end
 
   def lesson03
+    @rosary = get_item("shan_m.001")
+    @card = get_item("shan_d.127")
+    @let1 = get_item("shan_L198.meta")
+    @let2 = get_item("shan_L196.meta")
+
     @title = t "learn.lesson03.title"
   end
 
   def lesson04
+    @workers = get_item("shan_p.239")
+    @chickens = get_item("shan_p.207")
+    @let1 = get_item("shan_L156.meta")
+    @let2 = get_item("shan_L002.meta")
+
     @title = t "learn.lesson04.title"
   end
 
   def lesson05
+    @const = get_item("shan_d.059")
+    @const_image = "shan_d.103.jpg"
+
     @title = t "learn.lesson05.title"
   end
 
