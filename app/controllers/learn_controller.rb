3e4d3f63991d4d62084b8930af2b5141f23d964f
Jessica Dussault <jdussault4@gmail.com>
2019-11-01 12:36:42 -0500

    updating ids for lessons to new system

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -6,43 +6,43 @@ class LearnController < ApplicationController
   end
 
   def lesson01
-    @xmas = get_item("shan_p.135")
-    @tv = get_item("shan_p.153")
-    @stamps = get_item("shan_m.026")
-    @letter = get_item("shan_L094.meta")
+    @xmas = get_item("shan.P067")
+    @tv = get_item("shan.P076")
+    @stamps = get_item("shan.M010")
+    @letter = get_item("shan.L094")
 
     @title = t "learn.lesson01.title"
   end
 
   def lesson02
-    @wed1 = get_item("shan_p.257")
-    @wed2 = get_item("shan_p.073")
-    @wed3 = get_item("shan_p.067")
-    @letter = get_item("shan_L218.meta")
+    @wed1 = get_item("shan.P128")
+    @wed2 = get_item("shan.P036")
+    @wed3 = get_item("shan.P033")
+    @letter = get_item("shan.L218")
 
     @title = t "learn.lesson02.title"
   end
 
   def lesson03
-    @rosary = get_item("shan_m.001")
-    @card = get_item("shan_d.127")
-    @let1 = get_item("shan_L198.meta")
-    @let2 = get_item("shan_L196.meta")
+    @rosary = get_item("shan.M001")
+    @card = get_item("shan.D008")
+    @let1 = get_item("shan.L198")
+    @let2 = get_item("shan.L196")
 
     @title = t "learn.lesson03.title"
   end
 
   def lesson04
-    @workers = get_item("shan_p.239")
-    @chickens = get_item("shan_p.207")
-    @let1 = get_item("shan_L156.meta")
-    @let2 = get_item("shan_L002.meta")
+    @workers = get_item("shan.P119")
+    @chickens = get_item("shan.P103")
+    @let1 = get_item("shan.L156")
+    @let2 = get_item("shan.L002")
 
     @title = t "learn.lesson04.title"
   end
 
   def lesson05
-    @const = get_item("shan_d.059")
+    @const = get_item("shan.D005")
     @const_image = "shan_d.103.jpg"
 
     @title = t "learn.lesson05.title"
@@ -53,7 +53,7 @@ class LearnController < ApplicationController
   def get_item(id)
     item = {}
     item["res"] = $api.get_item_by_id(id).first
-    if item["res"]["uri_html"]
+    if item["res"] && item["res"]["uri_html"]
       url = item["res"]["uri_html"]
       item["html"] = Net::HTTP.get(URI.parse(url)) if url
     end
