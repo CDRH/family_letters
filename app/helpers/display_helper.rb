b88c5dd1fbe269b9997a028f812957c50ce30e0d
Jessica Dussault <jdussault4@gmail.com>
2019-11-25 11:39:45 -0600

    drops api field category and renames subcategory
    subcategory is now "category" in terms of labels
    that the user will see
    changes on browse / search / show pages
    closes https://github.com/CDRH/family_letters/issues/141
    
    temporarily overrides orchid method because of a bug
    discovered about displaying non-linked metadata field values

--- app/helpers/display_helper.rb
+++ app/helpers/display_helper.rb
@@ -1,4 +1,27 @@
 module DisplayHelper
   include Orchid::DisplayHelper
 
+  # TODO this is a temporary override of the orchid method
+  # and can be removed once https://github.com/CDRH/orchid/pull/173
+  # becomes part of a release
+  def metadata(res, label, api_field, link: true, separator: " | ")
+    data = metadata_get_field_values(res, api_field)
+    if data.present?
+      html = metadata_label(label, length: data.length)
+      # iterate through the field values
+      dataArray = data.map do |item|
+        if link
+          metadata_create_field_link(api_field, item)
+        else
+          value_label(api_field, item)
+        end
+      end
+      html << dataArray
+                .map { |i| "<span>#{i}</span>" }
+                .join(separator)
+
+      sanitize html
+    end
+  end
+
 end
