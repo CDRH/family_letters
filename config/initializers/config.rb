baaecf8d241aade3e13f30d97f8dd13bd29dfd4f
William Dewey <wdewey2@unl.edu>
2024-11-06 09:51:54 -0600

    run rails app:upgrade for rails 6

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,7 +1,7 @@
 # config/initializers/config.rb
 
-PUBLIC = YAML.load_file("#{Rails.root}/config/public.yml")[Rails.env]
-PRIVATE = YAML.load_file("#{Rails.root}/config/private.yml")[Rails.env]
+PUBLIC = YAML.load_file("#{Rails.root}/config/public.yml", aliases: true)[Rails.env]
+PRIVATE = YAML.load_file("#{Rails.root}/config/private.yml", aliases: true)[Rails.env]
 
 VERSION = PUBLIC["app_options"]["version"]
 
@@ -27,7 +27,7 @@ if APP_OPTS.key?("sections")
     config_path = Rails.root.join("config", "sections", "#{name}.yml")
 
     if File.exists?(config_path)
-      SECTIONS[name] = YAML.load_file(config_path)[Rails.env]
+      SECTIONS[name] = YAML.load_file(config_path, aliases: true)[Rails.env]
       SECTIONS[name]["name"] = name
     else
       raise "Section config file not found: #{config_path}"
